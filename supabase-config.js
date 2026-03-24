6cfccbe795c0b1aa5bb7343135ce930ddcb52b3c
mohammaddanish9343-coder <mohammaddanish9343@gmail.com>
2026-03-24 23:11:26 +0530

    Update Supabase

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
-window.SUPABASE_URL = 'https://fzhqakiiomqncitzojjn.supabase.co';
-window.SUPABASE_ANON_KEY = 'sb_publishable_jw2BDiBqucPA0yn9wcAy3A_LA0NRgxv';
+window.SUPABASE_URL = 'https://cdlcrhdenttfdrnaxouk.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_1MIkk4Mzz2LxxZXF9xDAZQ_z9_zrEWl';
 window.SUPABASE_USERS_TABLE = 'users';
